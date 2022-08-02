cf398811a01b0fa9d9556aab6628c17dfd93cacb
andrey.yurchenko <andrey.yurchenko1234@gmail.com>
2022-08-02 18:15:27 +0300

    add eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <header>
+        <h1>To Do List</h1>
+      </header>
       <main>
-        text
+        <div className='app__wrapper'>
+          
+        </div>
       </main>
     </div>
   );
