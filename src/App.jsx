4b8e73c4d3588f0c245a9398bb85c292b0595640
andrey.yurchenko <andrey.yurchenko1234@gmail.com>
2022-08-03 17:43:17 +0300

    approximate view app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,30 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="app">
-      <header>
+    <div className='app'>
+      <header className='app__header'>
         <h1>To Do List</h1>
       </header>
       <main>
-        <div className='app__wrapper'>
-          
+        <div className='to-do'>
+          <div className='to-do__panel'>
+            <div className='to-do-item'>
+              <div className='to-do-item__text'>
+                <p>TODO</p>
+              </div>
+            </div>
+            <button className='to-do__btn btn'>New TODO</button>
+          </div>
+          <div className='to-do__edit'>
+            <div className='edit'>
+              <h2>Edit todo</h2>
+              <div className='edit__container'>
+                <p>title</p>
+                <p>time</p>
+              </div>
+            </div>
+            
+          </div>
         </div>
       </main>
     </div>
