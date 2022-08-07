6e0849c7fc0e0835bff13a1f2de309ee188411f8
andrey.yurchenko <andrey.yurchenko1234@gmail.com>
2022-08-07 17:06:14 +0300

    Add color indication of TODO progress status

--- src/components/todos/Todo.jsx
+++ src/components/todos/Todo.jsx
@@ -3,13 +3,37 @@ import PropTypes from 'prop-types';
 
 import './Todo.scss';
 
+const PROGRESS_STATUSES = {
+  awaiting: 'Ожидает',
+  inProgress: 'В процессе',
+  completed: 'Выполнена',
+};
+
 export default function Todo({item, activeItem, setActiveItem}) {
+  const getClassNames = () => {
+    let result;
+    switch (item.progress) {
+    case PROGRESS_STATUSES.awaiting: {
+      result = item.id !== activeItem.id ? 'to-do-item__awaiting' : 'to-do-item__awaiting to-do-item__awaiting-active';
+      break;
+    }
+    case PROGRESS_STATUSES.inProgress: {
+      result = item.id !== activeItem.id ? 'to-do-item__in-progress' : 'to-do-item__in-progress to-do-item__in-progress-active';
+      break;
+    }
+    default: {
+      result = item.id !== activeItem.id ? 'to-do-item__completed' : 'to-do-item__completed to-do-item__completed-active';
+      break;
+    }
+    }
+    return result;
+  };
   const onClickToDo = (item) => {
     setActiveItem(item);
   };
   return (
-    <div className={`to-do-item ${item.id === activeItem.id ? 'to-do-item__active' : ''}`} onClick={() => onClickToDo(item)}>
-      <div className='to-do-item__text'>
+    <div className={'to-do-item }'} onClick={() => onClickToDo(item)}>
+      <div className={`to-do-item__text ${getClassNames()}`}>
         <p>{item.title}</p>
       </div>
     </div>
